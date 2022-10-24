35cb3c8f89619cb9decf5177d114ea80211f7663
pixincreate <pavananarayana01@gmail.com>
2022-10-24 22:07:50 +0530

    added error(), a lot of exception handlings that includes driver errors and others.

--- driver_check.py
+++ driver_check.py
@@ -21,6 +21,7 @@ import sys
 
 import winapps
 import traceback
+import webbrowser
 
 from selenium import webdriver
 
@@ -29,6 +30,8 @@ from selenium.webdriver.chrome.service import Service
 
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
+
+from selenium.common.exceptions import WebDriverException
 
 
 # Provides the facility to run on both terminal as well as in the python console
@@ -40,70 +43,91 @@ def resource_path(another_way):
     return os.path.join(usual_way, another_way)
 
 
+def error():
+    print("Webdriver seems to be outdated, try updating the webdriver in correspondence to your browser version..",
+          end='\n')
+    webbrowser.open('https://chromedriver.chromium.org/downloads', new=2)
+
+
 # Functionality
 def web_driver(head_mode):
+    # Fetches the application list from your installed apps
+    for item in winapps.list_installed():
+        item = item.name
+        if 'Brave Nightly' in item:
+            flag = 'Brave-Browser-Nightly'
+        elif 'Brave' in item:
+            flag = 'Brave-Browser'
+        if 'Chrome' in item:
+            flag = 'Chrome'
+        elif 'Firefox' in item:
+            flag = 'Firefox'
+
+    options = Options()
+    # Checks for Chromium based browser i.e., Brave
+    if 'Brave-Browser' == flag or 'Brave-Browser-Nightly' == flag:
+        options.binary_location = f"C:\\Program Files\\BraveSoftware\\{flag}\\Application\\brave.exe"
+        is_chromium = True
+
+    # Checks for the existence of Firefox
+    elif 'Firefox' == flag:
+        options = FirefoxOptions()
+
+    # Checks whether the browser selected is Google Chrome which I do not recommend using it whatsoever
+    elif 'Chrome' == flag:
+        print("Google Chrome is NOT RECOMMENDED due to it's stance on user privacy.\n"
+              "PiX recommends you using Firefox or Brave instead.\n"
+              "Also, as per the reports, Chrome is the most insecure browser to be existent!")
+        is_chromium = True
+
+    # Edge is unsupported as I want to take revenge against Microsoft, LOL
+    elif 'Edge' == flag:
+        print("Edge is unsupported because PiX don't use it!")
+
+    # Rare condition!
+    else:
+        print("No browser is supported on your PC."
+              "Kindly install 'Brave' or 'Firefox' to get this module working!")
+
+    if head_mode == 'headless':
+        options.add_argument(f"--{head_mode}")
+
     try:
-        # Fetches the application list from your installed apps
-        for item in winapps.list_installed():
-            item = item.name
-            if 'Brave Nightly' in item:
-                flag = 'Brave-Browser-Nightly'
-            elif 'Brave' in item:
-                flag = 'Brave-Browser'
-            if 'Chrome' in item:
-                flag = 'Chrome'
-            elif 'Firefox' in item:
-                flag = 'Firefox'
-
-        options = Options()
-        # Checks for Chromium based browser i.e., Brave
-        if 'Brave-Browser' == flag or 'Brave-Browser-Nightly' == flag:
-            options.binary_location = f"C:\\Program Files\\BraveSoftware\\{flag}\\Application\\brave.exe"
-            is_chromium = True
-
-        # Checks for the existence of Firefox
-        elif 'Firefox' == flag:
-            options = FirefoxOptions()
-
-        # Checks whether the browser selected is Google Chrome which I do not recommend using it whatsoever
-        elif 'Chrome' == flag:
-            print("Google Chrome is NOT RECOMMENDED due to it's stance on user privacy.\n"
-                  "PiX recommends you using Firefox or Brave instead.\n"
-                  "Also, as per the reports, Chrome is the most insecure browser to be existent!")
-            is_chromium = True
-
-        # Edge is unsupported as I want to take revenge against Microsoft, LOL
-        elif 'Edge' == flag:
-            print("Edge is unsupported because PiX don't use it!")
-
-        # Rare condition!
-        else:
-            print("No browser is supported on your PC."
-                  "Kindly install 'Brave' or 'Firefox' to get this module working!")
-
-        if head_mode == 'headless':
-            options.add_argument(f"--{head_mode}")
-
         # If the webdriver is up to-date
-        try:
-            # and it is chromium (blink) based browser
-            if is_chromium:
-                options.add_argument("--incognito")
-                driver = webdriver.Chrome(service=Service(executable_path=resource_path('web_drivers\\chromedriver.exe')),
-                                          options=options)
-            # if the browser is based on Firefox
-            else:
-                options.add_argument("--private")
+        # and it is chromium (blink) based browser
+        if is_chromium:
+            options.add_argument("--incognito")
+            try:
+                driver = webdriver.Chrome(
+                    service=Service(executable_path=resource_path('web_drivers\\chromedriver.exe')),
+                    options=options)
+            # If the Webdriver is outdated or it doesn't exist!
+            except FileNotFoundError:
+                error()
+            except WebDriverException:
+                error()
+        # if the browser is based on Firefox
+        else:
+            options.add_argument("--private")
+            try:
                 driver = webdriver.Firefox(service=FirefoxService(
                     executable_path=resource_path('web_drivers\\geckodriver.exe')),
                     options=options)
-        # If the Webdriver is outdated or it doesn't exist!
-        except FileNotFoundError:
-            print("Webdriver seems to be outdated, try updating the webdriver..", end='\n')
-
+            # If the Webdriver is outdated or it doesn't exist!
+            except FileNotFoundError:
+                error()
+            except WebDriverException:
+                error()
     except Exception as e:
         driver.quit()
-        print(e)
+        print('Error: ', e)
         traceback.print_exc()
+    try:
+        return driver
+    except UnboundLocalError:
+        quit()
 
-    return driver
+
+web = web_driver('lol')
+web.get('https://pixincreate.github.io')
+web.quit()
